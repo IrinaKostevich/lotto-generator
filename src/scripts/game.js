c87ea2b82d6a0b3cb4aa6f0160ed663814dfab26
Irina Kostevich <irinakostevich@Irinas-MacBook-Pro.local>
2019-06-08 16:48:51 +1000

    slider added

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,44 +1,61 @@
 import getLottos from './services/lotto.js';
 import generateRandomGame from './services/game.js';
 import { createBallsListElement } from './ui/render-page.js';
-import { createDomOptions } from './utils/dom-utils.js'
+import { createDomOptions } from './utils/dom-utils.js';
+
+const selectorContainer = document.getElementById('js-lotto-game-select');
+const rangeInputElement = document.getElementById('js-range-input');
+const rangeOutputElement = document.getElementById('js-range-output');
+const lottoContainer = document.getElementById('js-lotto-container');
 
 const lottoConfigs = new Map();
 
 export async function getGameOptions() {
-    // call lotto service
     const lottos = await getLottos();
 
-    // save lotto configs
     for (const lotto of lottos) {
         lottoConfigs.set(lotto.id, lotto.config);
     }
 
-    // create options list
     const optionsList = createDomOptions(lottos, lotto => lotto.id, lotto => lotto.name);
 
-    // insert options into select dropdown
-    const selectorContainer = document.getElementById('js-lotto-game-select');
     selectorContainer.append(...optionsList);
 }
 
-export async function generateGame(optionNumber) {
-    // get current config
+export async function generateGame(optionNumber, numbersCount) {
     const currentConfig = lottoConfigs.get(Number(optionNumber));
+    currentConfig.numbersCount = Number(numbersCount);
 
-    // generate new combination of numbers
     const lottoSet = await generateRandomGame(currentConfig);
-
-    // create html elements with numbers
     const lottoElement = createBallsListElement(lottoSet);
-
-    const lottoContainer = document.getElementById('js-lotto-container');
 
     // clear old numbers, if there are any
     if (lottoContainer.childNodes.length !== 0) {
         lottoContainer.removeChild(lottoContainer.firstChild);
     }
 
-    // insert new numbers on the page
     lottoContainer.append(lottoElement);
+}
+
+function getBallsDefaultCount(selectedItemValue) {
+    const currentConfig = lottoConfigs.get(Number(selectedItemValue));
+    const ballsDefaultCount = currentConfig.numbersCount;
+
+    return ballsDefaultCount;
+}
+
+function getConfigMaxNumber(selectedItemValue) {
+    const currentConfig = lottoConfigs.get(Number(selectedItemValue));
+    const configMaxNumber = currentConfig.maxNumber;
+
+    return configMaxNumber;
+}
+
+export function updateDefaultRange(selectedItemValue) {
+    const value = getBallsDefaultCount(selectedItemValue);
+    const maxValue = Math.floor(getConfigMaxNumber(selectedItemValue) / 2);
+
+    rangeInputElement.setAttribute('value', value);
+    rangeInputElement.setAttribute('max', maxValue);
+    rangeOutputElement.value = value;
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,35 @@
-import { getGameOptions, generateGame } from './game.js';
+import { getGameOptions, generateGame, updateDefaultRange } from './game.js';
 import { makeOptionSelected, getCurrentOption } from './utils/dom-utils.js';
 
 const lottoGameSelectElement = document.getElementById('js-lotto-game-select');
+const rangeInputElement = document.getElementById('js-range-input');
+const rangeOutputElement = document.getElementById('js-range-output');
 const nextButton = document.getElementById('js-next-button');
 
 function dropdownHandler(event) {
-    const selectElement = event.target;
-    const optionNumber = selectElement.value;
+    const selectedItem = event.target;
+    const selectedItemValue = selectedItem.value;
 
-    makeOptionSelected(selectElement, optionNumber);
-    generateGame(optionNumber);
+    makeOptionSelected(selectedItem, selectedItemValue);
+    updateDefaultRange(selectedItemValue);
+
+    const numbersCount = rangeInputElement.value;
+    generateGame(selectedItemValue, numbersCount);
+}
+
+function rangeInputHandler(event) {
+    rangeOutputElement.value = event.target.value;
 }
 
 function nextButtonHandler() {
-    const optionSelectedNumber = getCurrentOption(lottoGameSelectElement).value;
-    generateGame(optionSelectedNumber);
+    const selectedItemValue = getCurrentOption(lottoGameSelectElement).value;
+    const numbersCount = rangeInputElement.value;
+
+    generateGame(selectedItemValue, numbersCount);
 }
 
 getGameOptions()
     .then(lottoGameSelectElement.addEventListener('change', event => dropdownHandler(event)))
+    .then(rangeInputElement.addEventListener('change', event => rangeInputHandler(event)))
     .then(nextButton.addEventListener('click', event => nextButtonHandler(event)))
     .catch(error => console.error(error.name, error.details));
